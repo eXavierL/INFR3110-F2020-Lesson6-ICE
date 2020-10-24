2fae81eb1e7a51473ed7be42f0dcfbbffa65d7a2
Xavier <xavier.law@ontariotechu.net>
2020-10-23 22:05:17 -0400

    In Class
    Followed along with tutorial and submitting what was finished.

---
+++ Assets/_Scripts/TileManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TileManager : MonoBehaviour
+{
+    //Declare Queue of type GameObject
+    private Queue<GameObject> m_TilePool;
+
+    //Declare int for max tiles
+    public int MaxTiles;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Init Tile Pool
+        m_TilePool = new Queue<GameObject>();
+        _BuildTilePool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void _BuildTilePool()
+    {
+        for (var count = 0; count < MaxTiles; count++)
+        {
+            //Enqueue a new Tile from the Factory
+            var tempTile = TileFactory.Instance().CreateTile();
+            tempTile.SetActive(false);
+            m_TilePool.Enqueue(tempTile);
+        }
+    }
+
+    /// <summary>
+    /// Removes a GameObject Tile from the Pool
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetTile()
+    {
+        var tempTile = m_TilePool.Dequeue();
+        tempTile.SetActive(true);
+        return tempTile;
+    }
+
+    /// <summary>
+    /// Returns a GameObject Tile back into the Pool
+    /// </summary>
+    /// <param name="tile"></param>
+    public void ReturnTile(GameObject tile)
+    {
+        tile.SetActive(false);
+        m_TilePool.Enqueue(tile);
+    }
+}
